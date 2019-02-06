90c69630ff383c4027a1b0fa76aec2491830fd6a
etienne carriere <etienne.carriere@st.com>
2019-02-06 10:24:48 +0100

    stm32mp1: shared registers access
    Define a spinlock for shared registers access. This lock shall be
    used only for read/write operations on SoC registers.
    
    Signed-off-by: Etienne Carriere <etienne.carriere@st.com>

--- core/arch/arm/plat-stm32mp1/shared_resources.c
+++ core/arch/arm/plat-stm32mp1/shared_resources.c
@@ -4,7 +4,30 @@
  */
 
 #include <drivers/stm32mp1_clk.h>
+#include <io.h>
+#include <kernel/spinlock.h>
 #include <stm32_util.h>
+
+static unsigned int shregs_lock = SPINLOCK_UNLOCK;
+
+uint32_t lock_stm32shregs(void)
+{
+	return may_spin_lock(&shregs_lock);
+}
+
+void unlock_stm32shregs(uint32_t exceptions)
+{
+	may_spin_unlock(&shregs_lock, exceptions);
+}
+
+void io_mask32_stm32shregs(uintptr_t addr, uint32_t value, uint32_t mask)
+{
+	uint32_t exceptions = lock_stm32shregs();
+
+	io_mask32(addr, value, mask);
+
+	unlock_stm32shregs(exceptions);
+}
 
 void stm32_clock_enable(unsigned long id)
 {

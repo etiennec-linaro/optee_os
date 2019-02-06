72b710f8624bbbefea9b6f02a586356ac2b3a025
etienne carriere <etienne.carriere@st.com>
2019-02-06 10:24:22 +0100

    stm32mp1: util for shared resources refcount
    Implement {incr|decr}_shrefcnt(refcount, secure_flag) to provide
    reference counting for secure, non-secure and resources used both
    from secure and non-secure world.
    
    Functions {incr|decr}_refcnt(refcount) is a secure refcount only
    reference counting.
    
    Signed-off-by: Etienne Carriere <etienne.carriere@st.com>

--- core/arch/arm/plat-stm32mp1/stm32_util.h
+++ core/arch/arm/plat-stm32mp1/stm32_util.h
@@ -6,6 +6,8 @@
 #ifndef __STM32_UTIL_H__
 #define __STM32_UTIL_H__
 
+#include <assert.h>
+#include <kernel/panic.h>
 #include <stdint.h>
 
 /* Backup registers and RAM utils */
@@ -39,5 +41,65 @@ void stm32_clock_enable(unsigned long id);
 void stm32_clock_disable(unsigned long id);
 unsigned long stm32_clock_get_rate(unsigned long id);
 bool stm32_clock_is_enabled(unsigned long id);
+
+/*
+ * Shared reference counter: increments by 2 on secure increment
+ * request, decrements by 2 on secure decrement request. Bit #0
+ * is set to 1 on non-secure increment request and reset to 0 on
+ * non-secure decrement request. These counters initialize to
+ * either 0, 1 or 2 upon their expect default state.
+ * Counters saturate to UINT_MAX / 2.
+ */
+#define SHREFCNT_NONSECURE_FLAG		0x1ul
+#define SHREFCNT_SECURE_STEP		0x2ul
+#define SHREFCNT_MAX			(UINT_MAX / 2)
+
+/* Return 1 if refcnt increments from 0, else return 0 */
+static inline int incr_shrefcnt(unsigned int *refcnt, bool secure)
+{
+	int rc = !*refcnt;
+
+	if (secure) {
+		if (*refcnt < SHREFCNT_MAX) {
+			*refcnt += SHREFCNT_SECURE_STEP;
+			assert(*refcnt < SHREFCNT_MAX);
+		}
+	} else {
+		*refcnt |= SHREFCNT_NONSECURE_FLAG;
+	}
+
+	return rc;
+}
+
+/* Return 1 if refcnt decrements to 0, else return 0 */
+static inline int decr_shrefcnt(unsigned int *refcnt, bool secure)
+{
+	int  rc = 0;
+
+	if (secure) {
+		if (*refcnt < SHREFCNT_MAX) {
+			if (*refcnt < SHREFCNT_SECURE_STEP)
+				panic();
+
+			*refcnt -= SHREFCNT_SECURE_STEP;
+			rc = !*refcnt;
+		}
+	} else {
+		rc = (*refcnt == SHREFCNT_NONSECURE_FLAG);
+		*refcnt &= ~SHREFCNT_NONSECURE_FLAG;
+	}
+
+	return rc;
+}
+
+static inline int incr_refcnt(unsigned int *refcnt)
+{
+	return incr_shrefcnt(refcnt, true);
+}
+
+static inline int decr_refcnt(unsigned int *refcnt)
+{
+	return decr_shrefcnt(refcnt, true);
+}
 
 #endif /*__STM32_UTIL_H__*/

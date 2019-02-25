1a30799e1c7abba44f08b10d022897e9cddd6f9f
Etienne Carriere <etienne.carriere@st.com>
2019-02-25 15:29:18 +0100

    [review] stm32_rng: driver for STM32 RNG
    Add SMP protection on RNG accesses through stm32_rng_read().
    
    - factorize RNG context setup in local gate_rng().
    
    - stm32_rng_read_raw() generates up the 16 random bytes. It
      executes with interrupt masked to prevent TEE thread from suspending
      so that timeout in producing samples is properly detected.
    
    - stm32_rng_read() fills the target output buffer by burst of
      16byte.
    
    Signed-off-by: Etienne Carriere <etienne.carriere@st.com>

--- core/drivers/stm32_rng.c
+++ core/drivers/stm32_rng.c
@@ -39,12 +39,14 @@
 struct stm32_rng_instance {
 	struct io_pa_va base;
 	unsigned long clock;
+	unsigned int lock;
+	unsigned int refcount;
 };
 
 static struct stm32_rng_instance *stm32_rng;
 
 /*
- * Extract from the STM32 RNG specification:
+ * Extracts from the STM32 RNG specification:
  *
  * When a noise source (or seed) error occurs, the RNG stops generating
  * random numbers and sets to “1” both SEIS and SECS bits to indicate
@@ -58,7 +60,7 @@ static struct stm32_rng_instance *stm32_rng;
  * 3. Confirm that SEIS is still cleared. Random number generation is
  * back to normal.
  */
-static bool conceal_seed_error(vaddr_t rng_base)
+static void conceal_seed_error(vaddr_t rng_base)
 {
 	if (read32(rng_base + RNG_SR) & (RNG_SR_SECS | RNG_SR_SEIS)) {
 		size_t i = 0;
@@ -70,74 +72,115 @@ static bool conceal_seed_error(vaddr_t rng_base)
 
 		if (read32(rng_base + RNG_SR) & RNG_SR_SEIS)
 			panic("RNG noise");
-
-		return true;
 	}
-
-	return false;
 }
 
-TEE_Result stm32_rng_read_raw(vaddr_t rng_base, uint8_t *out, size_t size)
+#define RNG_FIFO_BYTE_DEPTH		16u
+
+TEE_Result stm32_rng_read_raw(vaddr_t rng_base, uint8_t *out, size_t *size)
 {
-	uint8_t *buf = out;
-	size_t len = size;
+	bool enabled = false;
 	TEE_Result rc = TEE_ERROR_SECURITY;
-	int count = 0;
+	uint32_t exceptions = thread_mask_exceptions(THREAD_EXCP_ALL);
+	uint64_t timeout_ref = timeout_init_us(RNG_TIMEOUT_US);
 
-	/* Enable RNG if not, clock error is disabled */
-	if (!(read32(rng_base + RNG_CR) & RNG_CR_RNGEN))
+	if (!(read32(rng_base + RNG_CR) & RNG_CR_RNGEN)) {
+		/* Enable RNG if not, clock error is disabled */
 		write32(RNG_CR_RNGEN | RNG_CR_CED, rng_base + RNG_CR);
+		enabled = true;
+	}
 
-	while (len) {
-		uint64_t timeout_ref = timeout_init_us(RNG_TIMEOUT_US);
+	/* Wait RNG has produced well seeded random samples */
+	while (!timeout_elapsed(timeout_ref)) {
+		conceal_seed_error(rng_base);
 
-		/* Wait RNG has produced random well seeded samples */
-		while (!timeout_elapsed(timeout_ref)) {
-			if (!conceal_seed_error(rng_base) &&
-			    read32(rng_base + RNG_SR) & RNG_SR_DRDY)
-				break;
-		}
-		if (conceal_seed_error(rng_base))
-			continue;
-		if (!(read32(rng_base + RNG_SR) & RNG_SR_DRDY))
+		if (read32(rng_base + RNG_SR) & RNG_SR_DRDY)
 			break;
+	}
 
-		/* RNG is ready: read data by 32bit word, up to 4 words */
-		for (count = 0; count < 4; count++) {
+	if (read32(rng_base + RNG_SR) & RNG_SR_DRDY) {
+		uint8_t *buf = out;
+		size_t req_size = MIN(RNG_FIFO_BYTE_DEPTH, *size);
+		size_t len = req_size;
+
+		/* RNG is ready: read up to 4 32bit words */
+		while (len) {
 			uint32_t data32 = read32(rng_base + RNG_DR);
+			size_t sz = MIN(len, sizeof(uint32_t));
 
-			memcpy(buf, &data32, MIN(len, sizeof(uint32_t)));
-			buf += MIN(len, sizeof(uint32_t));
-			len -= MIN(len, sizeof(uint32_t));
+			memcpy(buf, &data32, sz);
+			buf += sz;
+			len -= sz;
+		}
+		rc = TEE_SUCCESS;
+		*size = req_size;
+	}
 
-			if (!len) {
-				rc = TEE_SUCCESS;
-				break;
-			}
+	if (enabled)
+		write32(0, rng_base + RNG_CR);
+
+	thread_unmask_exceptions(exceptions);
+
+	return rc;
+}
+
+static void gate_rng(bool enable, struct stm32_rng_instance *dev)
+{
+	vaddr_t rng_base = io_pa_or_va(&dev->base);
+	uint32_t exceptions = may_spin_lock(&dev->lock);
+
+	if (enable) {
+		/* incr_refcnt return non zero if resource shall be enabled */
+		if (incr_refcnt(&dev->refcount)) {
+			stm32_clock_enable(dev->clock);
+			write32(0, rng_base + RNG_CR);
+			write32(RNG_CR_RNGEN | RNG_CR_CED, rng_base + RNG_CR);
+		}
+	} else {
+		/* decr_refcnt return non zero if resource shall be disabled */
+		if (decr_refcnt(&dev->refcount)) {
+			write32(0, rng_base + RNG_CR);
+			stm32_clock_disable(dev->clock);
 		}
 	}
 
-	/* Disable RNG */
-	write32(0, rng_base + RNG_CR);
-
-	return rc;
+	may_spin_unlock(&dev->lock, exceptions);
 }
 
 TEE_Result stm32_rng_read(uint8_t *out, size_t size)
 {
 	TEE_Result rc = 0;
+	uint32_t exceptions;
+	vaddr_t rng_base = io_pa_or_va(&stm32_rng->base);
+	uint8_t *out_ptr = out;
+	size_t out_size = 0;
 
 	if (!stm32_rng) {
 		DMSG("No RNG");
 		return TEE_ERROR_NOT_SUPPORTED;
 	}
 
-	stm32_clock_enable(stm32_rng->clock);
+	gate_rng(true, stm32_rng);
 
-	rc = stm32_rng_read_raw(io_pa_or_va(&stm32_rng->base), out, size);
+	while (out_size < size) {
+		/* Read by chunks of the size the RNG FIFO depth */
+		size_t sz = size - out_size;
 
-	stm32_clock_disable(stm32_rng->clock);
+		exceptions = may_spin_lock(&stm32_rng->lock);
 
+		rc = stm32_rng_read_raw(rng_base, out_ptr, &sz);
+
+		may_spin_unlock(&stm32_rng->lock, exceptions);
+
+		if (rc)
+			goto bail;
+
+		out_size += sz;
+		out_ptr += sz;
+	}
+
+bail:
+	gate_rng(false, stm32_rng);
 	if (rc)
 		memset(out, 0, size);
 
--- core/include/drivers/stm32_rng.h
+++ core/include/drivers/stm32_rng.h
@@ -20,12 +20,17 @@
 TEE_Result stm32_rng_read(uint8_t *out, size_t size);
 
 /*
- * As stm32_rng_read() but excluding clocks/reset dependencies
+ * As stm32_rng_read() but excluding clocks/reset dependencies.
+ *
  * @rng_base: Caller provides the RNG interface base address
  * @out: Output buffer
- * @size: Byte size of the output buffer
+ * @size: Pointer to input/output byte size of the output buffer
  * Return a TEE_Result compliant sttus
+ *
+ * When successfully returning, @size stores the number of bytes
+ * effectively generated in the output buffer @out. The input value
+ * of @size gives the size available in buffer @out.
  */
-TEE_Result stm32_rng_read_raw(vaddr_t rng_base, uint8_t *out, size_t size);
+TEE_Result stm32_rng_read_raw(vaddr_t rng_base, uint8_t *out, size_t *size);
 
 #endif /*__STM32_RNG_H__*/

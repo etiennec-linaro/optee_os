a439df4cf937776c67bc88024eac0eca6c4fdf00
Etienne Carriere <etienne.carriere@linaro.org>
2019-11-04 08:46:15 +0100

    scmi-server: fwk defines max_align_t for BUILD_OPTEE
    When BUILD_OPTEE is enabled, fwk_align.h provides a typedef for
    max_align_t expected by SCP-firmware.
    
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>

--- core/scmi-server/SCP-firmware/framework/include/fwk_align.h
+++ core/scmi-server/SCP-firmware/framework/include/fwk_align.h
@@ -22,4 +22,12 @@
 #   include <stdalign.h>
 #endif
 
+#ifdef BUILD_OPTEE
+#include <stddef.h>
+#include <stdint.h>
+#ifndef _GCC_MAX_ALIGN_T
+typedef uintmax_t max_align_t;
+#endif
+#endif /*BUILD_OPTEE*/
+
 #endif /* FWK_ALIGN_H */

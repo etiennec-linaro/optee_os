003b5814899edb9c2ce74e6fd6a18e5540a61642
Etienne Carriere <etienne.carriere@linaro.org>
2019-12-02 02:36:07 +0100

    drivers/scmi-msg: support for reset domain protocol
    Adds SCMI reset domain protocol support in the SCMI message drivers
    as defined in SCMI specification v2.0 [1]. Not all the messages
    defined in the specification are supported.
    
    Embedded upon CFG_SCMI_MSG_RESET_DOMAIN=y.
    
    scmi_msg_get_rd_handler() sanitizes the messsage_id value
    against any speculative use of reset domain ID as a index since by
    SCMI specification, IDs are indices.
    
    SCMI resource in this implementation are dumped or inspired by the
    SCP-firmware implementation [2] of the SCMI protocol, server side.
    
    Link: [1] http://infocenter.arm.com/help/topic/com.arm.doc.den0056a/DEN0056A_System_Control_and_Management_Interface.pdf
    Link: [2] https://github.com/ARM-software/SCP-firmware.git
    
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>

--- core/drivers/scmi-msg/sub.mk
+++ core/drivers/scmi-msg/sub.mk
@@ -3,3 +3,4 @@ srcs-$(CFG_SCMI_MSG_CLOCK) += clock.c
 srcs-y += entry.c
 srcs-$(CFG_ARM32_core) += platform_weaks_a32.S
 srcs-$(CFG_ARM64_core) += platform_weaks_a64.S
+srcs-$(CFG_SCMI_MSG_RESET_DOMAIN) += reset_domain.c

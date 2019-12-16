7582c7b4a3fe44c50dcb4fb862996886ed92507c
Etienne Carriere <etienne.carriere@linaro.org>
2019-12-16 11:44:01 +0100

    drivers/scmi-msg: smt entry points for incoming messages
    This change implements SCMI channels for reading a SCMI message from a
    shared memory and call the SCMI message drivers to route the message
    to the target platform services.
    
    SMT refers to the shared memory management protocol which is used
    to get/put message/response in shared memory. SMT is a 28byte header
    stating shared memory state and exchanged protocol data.
    
    The processing entry for a SCMI message can be a secure interrupt
    (CFG_SCMI_MSG_SMT_INTERRUPT_ENTRY=y), and fastcall SMC
    (CFG_SCMI_MSG_SMT_FASTCALL_ENTRY=y) or a threaded execution
    context entry (CFG_SCMI_MSG_SMT_THREAD_ENTRY=y).
    
    SMT description in this implementation is based on the SCP-firmware
    implementation [1].
    
    Link: [1] https://github.com/ARM-software/SCP-firmware.git
    
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>

--- core/drivers/scmi-msg/sub.mk
+++ core/drivers/scmi-msg/sub.mk
@@ -4,3 +4,4 @@ srcs-y += entry.c
 srcs-$(CFG_ARM32_core) += platform_weaks_a32.S
 srcs-$(CFG_ARM64_core) += platform_weaks_a64.S
 srcs-$(CFG_SCMI_MSG_RESET_DOMAIN) += reset_domain.c
+srcs-$(CFG_SCMI_MSG_SMT) += smt.c

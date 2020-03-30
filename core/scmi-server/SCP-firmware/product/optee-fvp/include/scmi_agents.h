a34475694a984b680435b1bf0dee13956f96e498
Etienne Carriere <etienne.carriere@linaro.org>
2020-03-30 11:09:20 +0200

    scmi-server: fvp: move to optee_mhu/smt for message exchange
    SCMI server in OP-TEE interfaces non-secure world through the optee_mhu
    and optee_smt modules.
    
    Replace optee-fvp product specific hmbx module and its configuration
    with optee_mhu/smt transport resources.
    
    Replace optee-fvp product specific spci_scmi.h with scmi_agents.h to
    define SCMI agent, service and channel IDs.
    
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>

---
+++ core/scmi-server/SCP-firmware/product/optee-fvp/include/scmi_agents.h
@@ -0,0 +1,32 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2015-2019, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef SCMI_AGENTS_H
+#define SCMI_AGENTS_H
+
+enum scmi_agent_id {
+    /* 0 is reserved for the platform */
+    SCMI_AGENT_ID_OSPM = 1,
+    SCMI_AGENT_ID_PSCI,
+    SCMI_AGENT_ID_COUNT,
+};
+
+enum scmi_service_idx {
+    SCMI_SERVICE_IDX_PSCI,
+    SCMI_SERVICE_IDX_OSPM_0,
+    SCMI_SERVICE_IDX_OSPM_1,
+    SCMI_SERVICE_IDX_COUNT,
+};
+
+enum scmi_channel_device_idx {
+    SCMI_CHANNEL_DEVICE_IDX_PSCI,
+    SCMI_CHANNEL_DEVICE_IDX_OSPM_0,
+    SCMI_CHANNEL_DEVICE_IDX_OSPM_1,
+    SCMI_CHANNEL_DEVICE_IDX_COUNT
+};
+
+#endif /* SCMI_AGENTS_H */

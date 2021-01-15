a8fa66cfdeccf93e51c02c6b93e357cbbbfdde16
Robin van der Gracht <robin@protonic.nl>
2021-01-15 09:26:59 +0100

    ta: pkcs11: Load persistent object attributes during db initialization
    Persistent object attributes are now loaded during the first find
    operation. This means that you're forced to do a find operation to make
    the objects available.
    
    Right now entry_find_objects_init is broken in such that it tries to
    access the obj->attributes for objects in the session->token->object_list
    before setting the obj->attributes (by calling token_obj_matches_ref).
    This results in a Panic.
    
    This patch fixes both issues.
    
    Signed-off-by: Robin van der Gracht <robin@protonic.nl>

--- ta/pkcs11/src/persistent_token.c
+++ ta/pkcs11/src/persistent_token.c
@@ -462,6 +462,76 @@ out:
 	return tee2pkcs_error(res);
 }
 
+static enum pkcs11_rc load_object_attribs(struct pkcs11_object *obj)
+{
+	enum pkcs11_rc rc;
+	TEE_Result res = TEE_ERROR_GENERIC;
+	TEE_ObjectHandle hdl = obj->attribs_hdl;
+	TEE_ObjectInfo info;
+	struct obj_attrs *attr = NULL;
+	uint32_t read_bytes = 0;
+
+	if (hdl != TEE_HANDLE_NULL) {
+		IMSG("Handle attributes already set for this object");
+		return PKCS11_CKR_OK;
+	}
+
+	res = TEE_OpenPersistentObject(TEE_STORAGE_PRIVATE,
+				       obj->uuid, sizeof(*obj->uuid),
+				       TEE_DATA_FLAG_ACCESS_READ, &hdl);
+	if (res) {
+		EMSG("OpenPersistent failed 0x%" PRIx32, res);
+		return tee2pkcs_error(res);
+	}
+
+	TEE_MemFill(&info, 0, sizeof(info));
+	res = TEE_GetObjectInfo1(hdl, &info);
+	if (res) {
+		EMSG("GetObjectInfo failed 0x%" PRIx32, res);
+		rc = tee2pkcs_error(res);
+		goto bail;
+	}
+
+	attr = TEE_Malloc(info.dataSize, TEE_MALLOC_FILL_ZERO);
+	if (!attr) {
+		rc = PKCS11_CKR_DEVICE_MEMORY;
+		goto bail;
+	}
+
+	res = TEE_ReadObjectData(hdl, attr, info.dataSize, &read_bytes);
+	if (!res) {
+		res = TEE_SeekObjectData(hdl, 0, TEE_DATA_SEEK_SET);
+		if (res)
+			EMSG("Seek to 0 failed 0x%" PRIx32, res);
+	}
+
+	if (res) {
+		rc = tee2pkcs_error(res);
+		EMSG("Read %" PRIu32 " bytes, failed 0x%" PRIx32,
+		     read_bytes, res);
+		goto bail;
+	}
+	if (read_bytes != info.dataSize) {
+		EMSG("Read %" PRIu32 " bytes, expected 0x%" PRIu32,
+		     read_bytes, info.dataSize);
+		rc = PKCS11_CKR_GENERAL_ERROR;
+		goto bail;
+	}
+
+	obj->attributes = attr;
+	attr = NULL;
+	obj->attribs_hdl = hdl;
+	hdl = TEE_HANDLE_NULL;
+	rc = PKCS11_CKR_OK;
+
+bail:
+	TEE_Free(attr);
+	if (obj->attribs_hdl == TEE_HANDLE_NULL && hdl != TEE_HANDLE_NULL)
+		TEE_CloseObject(hdl);
+
+	return rc;
+}
+
 /*
  * Return the token instance, either initialized from reset or initialized
  * from the token persistent state if found.
@@ -533,6 +603,9 @@ struct ck_token *init_persistent_db(unsigned int token_id)
 			obj = create_token_object(NULL, uuid);
 			if (!obj)
 				TEE_Panic(0);
+
+			if (load_object_attribs(obj) != PKCS11_CKR_OK)
+				EMSG("Unable to load obj attributes from db");
 
 			LIST_INSERT_HEAD(&token->object_list, obj, link);
 		}
